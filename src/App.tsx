1b0d5fc05b55ff543eb3ce5c35d3e7586d25e3e9
herfost <133514023+sbadung@users.noreply.github.com>
2023-07-28 20:50:27 +0200

    One page application

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Routes } from "react-router-dom";
 import FaqForm from "./components/Faq/FaqForm";
 import "./index.css";
 import Faq from "./pages/Faq";
@@ -7,8 +7,8 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/PlatformV2/form" element={<FaqForm />} />
-        <Route path="/PlatformV2/faq" element={<Faq />} />
+        <FaqForm />
+        <Faq />
       </Routes>
     </BrowserRouter>
   );
