4e4e90eb0262b85bdf98bffa9f199eca001c47b7
herfost <133514023+sbadung@users.noreply.github.com>
2023-07-28 20:54:54 +0200

    One page application

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import { BrowserRouter, Routes } from "react-router-dom";
 import FaqForm from "./components/Faq/FaqForm";
 import "./index.css";
 import Faq from "./pages/Faq";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <FaqForm />
-        <Faq />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <FaqForm />
+      <Faq />
+    </>
   );
 }
